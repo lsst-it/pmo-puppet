a545b36f08be3112f51d195500256ce74ea12d4f
Iain Goodenow <igoodenow@lsst.org>
2021-04-25 21:33:32 -0700

    Added txt to default; graylog pp; pdm2 new class

---
+++ site/profile/manifests/monitoring_graylog.pp
@@ -0,0 +1,20 @@
+# NX Log Agent
+# Requires Source MSI
+# Requires Source Conf file
+# Verify installed
+# Verify conf file exists and proper content
+# Verify service exists and is running
+class profile::monitoring_graylog {
+    #package { 'nxlog':
+    #    ensure => '2.10.2150',
+    #    source => '//fp1/IT/Installs/nxlog/nxlog-ce-2.10.2150.msi'
+    #}
+    file { 'C:/Program Files (x86)/nxlog/conf/nxlog.conf':
+        ensure => 'present',
+        source => '//fp1/IT/Installs/nxlog/nxlog1.conf'
+    }
+    service { 'nxlog':
+        ensure => 'running',
+        enable => true
+    }
+}
