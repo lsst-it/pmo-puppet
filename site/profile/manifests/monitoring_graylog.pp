62d705e46a18c85fbf742e5c2c669242c1f513e1
Iain Goodenow <igoodenow@lsst.org>
2021-04-25 22:29:37 -0700

    Works for DS1 and have installer ready

--- site/profile/manifests/monitoring_graylog.pp
+++ site/profile/manifests/monitoring_graylog.pp
@@ -5,11 +5,11 @@
 # Verify conf file exists and proper content
 # Verify service exists and is running
 class profile::monitoring_graylog {
-    #package { 'nxlog':
-    #    ensure => '2.10.2150',
-    #    source => '//fp1/IT/Installs/nxlog/nxlog-ce-2.10.2150.msi'
-    #}
-    file { 'C:/Program Files (x86)/nxlog/conf/1.conf':
+    package { 'nxlog':
+        ensure => '2.10.2150',
+        source => '//fp1/IT/Installs/nxlog/nxlog-ce-2.10.2150.msi'
+    }
+    file { 'C:/Program Files (x86)/nxlog/conf/nxlog.conf':
         ensure => 'present',
         source => '//fp1/IT/Installs/nxlog/nxlog.conf'
     }
