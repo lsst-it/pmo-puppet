61afec5c8eac17953be8ae320901024418bc3395
Shahram <wf@belldex.com>
2021-05-17 13:48:02 -0700

    Encrypting nagios pwd.

--- Puppetfile
+++ Puppetfile
@@ -11,6 +11,7 @@ mod 'dna-notepad_plus_plus', '0.1.1'
 mod 'puppetlabs-scheduled_task', '3.0.0'
 mod 'dylanratcliffe-windows_disk_facts', '0.2.7'
 mod 'puppet-nsclient', '2.1.0'
+mod 'puppet-download_file', '4.0.0'
 # AWS
 mod 'puppetlabs-aws', '2.1.0'
 mod 'jdowning-awscli', '2.2.0'
--- site/profile/manifests/nsclient.pp
+++ site/profile/manifests/nsclient.pp
@@ -3,13 +3,15 @@
 # It will be bumped up once testing is complete
 # Since 0.3.7 is a very old version, the newer versions' service name is changed to ncp.
 # as a result puppet run will throw an error but it should still work.
-class profile::nsclient {
+class profile::nsclient (Sensitive[String]
+$nagios_password
+){
   class { 'nsclient':
     package_source_location => 'https://github.com/mickem/nscp/releases/download/0.3.7',
     package_name            => 'NSClient++ (x64)',
     package_source          => 'NSClient.-0.3.7-x64.msi',
     allowed_hosts           => ['140.252.32.34'],
-    password                => testpwd,
+    password                => $nagios_password,
     service_enable          => true,
   }
 # Below service will be removed once we have upgraded to newer version of NSClient.
