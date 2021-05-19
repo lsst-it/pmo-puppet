9069844e4171f5267e39cf46dfda9107b2cace04
Shahram <wf@belldex.com>
2021-05-19 11:25:07 -0700

    remove a firewall rule

--- site/profile/manifests/grafana.pp
+++ site/profile/manifests/grafana.pp
@@ -16,11 +16,4 @@ class profile::grafana {
     ],
   }
   }
-
-  firewalld_port { 'Grafana Main Port':
-    ensure   => present,
-    port     => '3000',
-    protocol => 'tcp',
-    require  => Service['firewalld'],
-  }
 }
