065f69a1d54125e0864b45c0d707ba8d071b8732
Shahram <wf@belldex.com>
2021-05-19 13:36:39 -0700

    client agents for prometheus

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -1,5 +1,6 @@
 # Base profile for Linux OS
 class profile::base_linux {
+    include node_exporter
 Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
 'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
 'wget', 'nmap']:
---
+++ site/profile/manifests/base_windows.pp
@@ -0,0 +1,7 @@
+# Base profile for Windows OS
+class profile::base_windows {
+package { 'windows_exporter':
+    ensure => '0.16.0',
+    source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi'
+}
+}
