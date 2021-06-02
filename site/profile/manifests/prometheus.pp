12aed91acdefe61c2f40fa167e97c7b192f90226
Shahram <wf@belldex.com>
2021-06-02 06:40:10 -0700

    Adding blackbox

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -1,4 +1,17 @@
 # Prometheus monitoring URL: http://prometheus.us.lsst.org:9090/ 
 class profile::prometheus {
   include prometheus
+  class { 'prometheus::blackbox_exporter':
+    version => '0.19.0',
+    modules => {
+      'http_2xx' => {
+        'prober'  => 'http',
+        'timeout' => '5s',
+        'http'    => {
+          'valid_status_codes' => [],
+          'method'             => 'GET',
+        }
+      }
+    }
+  }
 }
