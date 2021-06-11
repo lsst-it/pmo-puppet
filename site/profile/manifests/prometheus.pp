58669905d7b77f07b6c7cee5d861dd0432878243
Shahram <wf@belldex.com>
2021-06-11 13:45:28 -0700

    add extra options

--- site/profile/manifests/prometheus.pp
+++ site/profile/manifests/prometheus.pp
@@ -21,7 +21,7 @@ $cluster_hide,
 # Alertmanager config
 class { 'prometheus::alertmanager':
   version       => '0.22.2',
-  extra_options => "--cluster.peer=${unwrap($cluster_hide)}",
+  extra_options => "--cluster.advertise-address=127.0.0.1:9093 \--cluster.listen-address=:9797 \--cluster.peer=${unwrap($cluster_hide)}",
   route         => {
     'group_by'        => ['job'],
     'group_wait'      => '30s', # how long to wait to buffer alerts of the same group before sending a notification initially
