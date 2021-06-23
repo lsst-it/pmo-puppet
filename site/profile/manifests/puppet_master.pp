c52a466d8aff06e4684d65d4e2a8dfd9d5026116
Shahram <wf@belldex.com>
2021-06-23 14:48:52 -0700

    notify puppetserver service

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -3,5 +3,6 @@ class profile::puppet_master {
   # The toml-rb gem is required for grafana ldap.
   package { 'toml-rb':
     provider => 'puppetserver_gem',
+    notify   => Service['puppetserver'],
   }
   }
