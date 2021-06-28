d0f9b712c130f0f763a780228ee7bca848545ddc
Shahram <wf@belldex.com>
2021-06-28 06:39:04 -0700

    en service status collection

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -2,16 +2,19 @@
 class profile::base_linux {
   include network
   include ::firewalld
-  include prometheus::node_exporter
-class { 'ntp':
-  servers => [ '140.252.1.140', '140.252.1.141' ],
-}
-class { 'timezone':
-    timezone => 'UTC',
-}
-Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
-'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
-'wget', 'nmap', 'iputils', 'bind-utils']:
-ensure => installed,
-}
+  class { 'prometheus::node_exporter':
+    version       => '1.1.2',
+    extra_options => '--collector.systemd \--collector.processes \--collector.meminfo_numa',
+  }
+  class { 'ntp':
+    servers => [ '140.252.1.140', '140.252.1.141' ],
+  }
+  class { 'timezone':
+      timezone => 'UTC',
+  }
+  Package { [ 'tree', 'tcpdump', 'telnet', 'lvm2', 'gcc', 'xinetd',
+  'bash-completion', 'sudo', 'screen', 'vim', 'openssl', 'openssl-devel',
+  'wget', 'nmap', 'iputils', 'bind-utils']:
+  ensure => installed,
+  }
 }
