930231e8481dd91628e0b2475d864ed7daf04cae
Shahram <wf@belldex.com>
2021-06-28 10:49:59 -0700

    added sshd_config

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -2,6 +2,7 @@
 class profile::base_linux {
   include network
   include ::firewalld
+  include ssh
 # config: /etc/systemd/system/node_exporter.service
   class { 'prometheus::node_exporter':
     version       => '1.1.2',
