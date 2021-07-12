9462ebf60e3640933437efb79446d436cbeb8d2d
Shahram <wf@belldex.com>
2021-07-12 11:24:20 -0700

    ensure win_exporter service is running

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -4,4 +4,8 @@ package { 'windows_exporter':
     ensure => '0.16.0',
     source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi'
 }
+# Start service if it has stopped or crashed.
+service { 'windows_exporter':
+  ensure => running,
+}
 }
