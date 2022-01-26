876012116df09e7e88c5aea1267bb560b3836b2c
Shahram <wf@belldex.com>
2022-01-26 13:27:36 -0700

    tomcat service

--- site/profile/manifests/tomcat.pp
+++ site/profile/manifests/tomcat.pp
@@ -31,5 +31,39 @@ $ciphers,
     roles         => ['admin-gui, manager-gui, manager-script'],
     catalina_base => $catalina_base,
   }
+# Getting tomcat::service to work was too painful
+  $tomcat_service = @("EOT")
+    [Unit]
+    Description=Apache Tomcat Web Application Container
+    After=syslog.target network.target
 
+    [Service]
+    Type=forking
+    SuccessExitStatus=143
+
+    Environment=JAVA_HOME=${java_home}
+    Environment=CATALINA_PID=${catalina_home}/temp/tomcat.pid
+    Environment=CATALINA_HOME=${catalina_home}
+    Environment=CATALINA_BASE=${catalina_base}
+    Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+    Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+
+    ExecStart=${catalina_home}/bin/startup.sh
+    ExecStop=${catalina_home}/bin/shutdown.sh
+
+    User=tomcat
+    Group=tomcat
+
+    [Install]
+    WantedBy=multi-user.target
+    | EOT
+# 
+  systemd::unit_file { 'tomcat.service':
+    content => "${tomcat_service}",
+  }
+  -> service { 'tomcat':
+  subscribe => Tomcat::Instance['default'],
+  ensure    => 'running',
+  enable    => true,
+  }
 }
