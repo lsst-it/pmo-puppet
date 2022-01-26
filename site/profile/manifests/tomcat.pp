cf42fcba28b8e4e08f32686f20d27589fe5541b9
Shahram <wf@belldex.com>
2022-01-26 11:53:06 -0700

    adding tomcat

---
+++ site/profile/manifests/tomcat.pp
@@ -0,0 +1,20 @@
+# tomcat 
+class profile::tomcat ( Sensitive[String]
+$tomcat_user_hide,
+$tomcat_pass_hide,
+$catalina_home,
+$catalina_base,
+$version,
+$java_home,
+$https_enabled,
+$keystorepass_hide,
+$ciphers,
+){
+
+  tomcat::install { $catalina_home:
+  source_url     => "https://dlcdn.apache.org/tomcat/${version}.tar.gz",
+  }
+  tomcat::instance { 'default':
+    catalina_home => $catalina_home,
+    catalina_base => $catalina_base,
+  }
