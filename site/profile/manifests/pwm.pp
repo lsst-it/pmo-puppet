e06228f2249757ddb7823581986364a3597fab48
Shahram <wf@belldex.com>
2022-01-28 09:30:26 -0700

    up archive version

--- site/profile/manifests/pwm.pp
+++ site/profile/manifests/pwm.pp
@@ -1,2 +1,10 @@
 # Reboot required following the initial run
-class profile::pwm {}
+class profile::pwm {
+
+  archive { '/tmp/pwm-1.9.2.war':
+    ensure   => present,
+    source   => 'https://github.com/pwm-project/pwm/releases/download/v1_9_2/pwm-1.9.2.war',
+    provider => 'wget',
+    cleanup  => false,
+  }
+}
