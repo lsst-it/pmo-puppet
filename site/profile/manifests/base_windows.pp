37550ad20c656c6f3db3694c13e09120f57949b2
shahramsobhani <wf@belldex.com>
2022-09-14 12:27:19 -0700

    Include Notpad++ package

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -5,7 +5,10 @@ class profile::base_windows {
       ensure => '0.19.0',
       source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
   }
-  # Start service if it has stopped or crashed.
+  package { 'Notepad++ (64-bit x64)':
+      ensure => '8.4.5',
+      source => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.5/npp.8.4.5.Installer.x64.exe'
+  }  # Start service if it has stopped or crashed.
   service { 'windows_exporter':
     ensure => running,
   }
