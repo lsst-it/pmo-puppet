cfff1bb2d5acc28d4ada106c98250121586a8c08
shahramsobhani <wf@belldex.com>
2022-09-14 11:30:28 -0700

    upgrade Win_exporter to 0.19.0

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -2,8 +2,8 @@
 class profile::base_windows {
   include chocolatey # Needed for just about most things for Windows.
   package { 'windows_exporter':
-      ensure => '0.16.0',
-      source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.16.0/windows_exporter-0.16.0-amd64.msi'
+      ensure => '0.19.0',
+      source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
   }
   # Start service if it has stopped or crashed.
   service { 'windows_exporter':
