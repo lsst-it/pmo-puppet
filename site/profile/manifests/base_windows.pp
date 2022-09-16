e5e1a54a6718e70d77f5519505f348acc89fa264
shahramsobhani <wf@belldex.com>
2022-09-16 07:27:44 -0700

    Change npp url

--- site/profile/manifests/base_windows.pp
+++ site/profile/manifests/base_windows.pp
@@ -5,11 +5,13 @@ class profile::base_windows {
       ensure => '0.19.0',
       source => 'https://github.com/prometheus-community/windows_exporter/releases/download/v0.19.0/windows_exporter-0.19.0-amd64.msi'
   }
-  package { 'Notepad++ (64-bit x64)':
-      ensure => '8.4.5',
-      source => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.4.5/npp.8.4.5.Installer.x64.exe'
-  }  # Start service if it has stopped or crashed.
+  # Start service if it has stopped or crashed.
   service { 'windows_exporter':
     ensure => running,
+  }
+  package { 'Notepad++ (64-bit x64)':
+      ensure          => installed,
+      source          => 'http://wsus.lsst.org/puppetfiles/notepad/Notepad7.9.1.msi',
+      install_options => '/quiet',
   }
 }
