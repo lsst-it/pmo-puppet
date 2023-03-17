0a93f5d0c99483ff115d6ed77f7763618d836dfb
shahramsobhani <wf@belldex.com>
2023-03-17 10:11:23 -0700

    rm monitoring_graylog.pp

--- site/profile/manifests/monitoring_graylog.pp
+++
@@ -1,8 +0,0 @@
-# NX Log Agent
-# Requires Source MSI
-# Requires Source Conf file
-# Verify installed
-# Verify conf file exists and proper content
-# Verify service exists and is running
-class profile::monitoring_graylog {
-}
