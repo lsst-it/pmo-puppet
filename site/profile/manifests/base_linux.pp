688b7f54d25db6b8f04ed3954946d1e9939553cb
shahramsobhani <wf@belldex.com>
2023-03-31 08:01:51 -0700

    set graylog to true

--- site/profile/manifests/base_linux.pp
+++ site/profile/manifests/base_linux.pp
@@ -2,7 +2,7 @@
 class profile::base_linux (
   Boolean $awscli  = false,
   Boolean $postfix = false,
-  Boolean $graylog = false,
+  Boolean $graylog = true,
 ) {
   include network
   include ::firewalld
