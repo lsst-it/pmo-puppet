c958bfdf1907121db30758a61f951b9bc9eb5f65
shahramsobhani <wf@belldex.com>
2023-08-16 13:18:06 -0700

    rm duplicate

--- site/profile/manifests/yourls.pp
+++ site/profile/manifests/yourls.pp
@@ -51,11 +51,6 @@ file { '/etc/nginx/YOURLS':
   target => "/etc/nginx/YOURLS-${yourls_version}",
 }
 
-  archive { '/tmp/mysql-db-yourls.gz' :
-    ensure  => present,
-    source  => 's3://yourls-data/yourls/20230816030002-mysql-db-yourls.gz',
-    cleanup => false,
-  }
   archive { '/tmp/yourls_config.zip' :
     ensure       => present,
     source       => 's3://yourls-data/yourls_config.zip',
