f70aba0bf4a6677c32697bdb70fd53be3d576c8f
shahramsobhani <wf@belldex.com>
2024-02-19 10:43:36 -0700

    add foreman role for new server

--- site/profile/manifests/puppet_master.pp
+++ site/profile/manifests/puppet_master.pp
@@ -5,4 +5,28 @@ class profile::puppet_master {
     ensure   => present,
     provider => 'puppetserver_gem',
   }
+  $prkpem = lookup('prkpem')
+  $pukpem = lookup('pukpem')
+  file {
+    '/etc/puppetlabs/puppet/eyaml':
+      ensure => directory,
+      owner  => 'puppet',
+      group  => 'puppet',
+      mode   => '0500',
+      ;
+    '/etc/puppetlabs/puppet/eyaml/private_key.pkcs7.pem':
+      ensure  => file,
+      owner   => 'puppet',
+      group   => 'puppet',
+      mode    => '0400',
+      content => $prkpem,
+      ;
+    '/etc/puppetlabs/puppet/eyaml/public_key.pkcs7.pem':
+      ensure  => file,
+      owner   => 'puppet',
+      group   => 'puppet',
+      mode    => '0400',
+      content => $pukpem,
+      ;
+  }
 }
